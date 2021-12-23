0242e685ffeac0d23ee55a7bec176b1fb32d136d
Biswadeep Dey <BiswadeepDey@eaton.com>
2021-12-23 21:15:12 +0530

    Create ctci project. Implemented URLify[1.3]

---
+++ src/array_strings/Urlify.java
@@ -0,0 +1,34 @@
+package array_strings;
+
+public class Urlify {
+	public static String convert(String str) {
+		char[] carr = str.toCharArray();
+		char[] regex = new char[] { '%', '2', '0' };
+		boolean isNonSpaceFound = false;
+		int s = carr.length - 1;
+		for (int i = s; i >= 0; i--) {
+			if (carr[i] != ' ') {
+				if (!isNonSpaceFound)
+					isNonSpaceFound = true;
+				carr[s] = carr[i];
+				s--;
+			} else {
+				if (isNonSpaceFound) {
+					int count = regex.length - 1;
+					while (count >= 0) {
+						carr[s] = regex[count];
+						count--;
+						s--;
+					}
+				}
+			}
+		}
+
+		return String.valueOf(carr);
+	}
+	
+	public static void main(String[] args)
+	{
+		System.out.println(convert("Mr John Smith    "));
+	}
+}
